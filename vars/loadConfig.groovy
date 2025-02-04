ed924852974939d2e80837ed495e8feebe40142b
prince98batra <prince98batra@gmail.com>
2025-02-04 17:55:40 +0530

    Update loadConfig.groovy

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -1,5 +1,7 @@
+
 def call() {
     // Load YAML file using the built-in readYaml step
     def configFile = libraryResource 'config.yml'  // Load the YAML file as a resource
     def yaml = readYaml text: configFile  // Parse the YAML content
     return yaml
+}
