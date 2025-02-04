01cbc6a7a1d8c8410a9bdf5eb18cbf164ac14fb5
prince98batra <prince98batra@gmail.com>
2025-02-04 17:55:18 +0530

    Update cloneRepo.groovy

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,16 +1,13 @@
 def call(String repoUrl) {
     def config = loadConfig()  // Load config file
     def branch = config.ENVIRONMENT == "prod" ? "main" : "develop"
+    
+    // Directory name (assumes the repo is named shared_library)
+    def dirName = 'shared_library'
+    
+    // Remove the existing directory if it exists
+    sh "rm -rf ${dirName}"
 
-    pipeline {
-        stages {
-            stage('Clone Repository') {
-                steps {
-                    script {
-                        sh "git clone -b ${branch} ${repoUrl}"
-                    }
-                }
-            }
-        }
-    }
+    // Clone the repository based on the branch
+    sh "git clone -b ${branch} ${repoUrl} ${dirName}"
 }
